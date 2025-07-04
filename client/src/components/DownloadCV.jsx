33ba51c3ce4cd4aed9745984220401ec331e32b5
Mehdi <128828181+Mehdi-MI@users.noreply.github.com>
2025-07-04 18:32:31 +0100

    Update DownloadCV.jsx

--- client/src/components/DownloadCV.jsx
+++ client/src/components/DownloadCV.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { FaDownload } from "react-icons/fa";
+import CVFile from "../public/hasbellaoui-mehdi-cv.pdf";
 
 const DownloadCV = () => {
   return (
     <a
-      href="/Hasbellaoui_Mehdi.pdf"
+      href={CVFile}
       download="Mehdi_CV.pdf"
       className="inline-flex items-center gap-2 bg-blue-600 text-white px-6 py-3 rounded-lg hover:bg-blue-700 transition-colors duration-300 font-medium"
     >
